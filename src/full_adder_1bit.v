b7a9c84a552d47316f5f7d85a5da33863912aca3
v1cc10 <victor.h.parada.m@gmail.com>
2026-09-06 13:20:22 -0300

    Comentarios sobre el Full_Adder de 1 bit
    Comentarios nuevos

--- src/full_adder_1bit.v
+++ src/full_adder_1bit.v
@@ -1,9 +1,9 @@
 module full_adder_1bit (
     input A,
     input B,
-    input Cin,
+    input Cin, //Agregamos soporte para un acarreo de antes que no estaba en el Half_adder (sumador_1bit.v)
     output S,
-    output Cout
+    output Cout //Y acá el accareo de salida del full adder
 );
 
     wire X;
@@ -15,7 +15,15 @@ module full_adder_1bit (
         .B(B),
         .S(X),
         .C(C1)
-    );
+    ); 
+    
+    //Cuando se llama a otro módulo, estamos llamando
+    //Como si fuera una función multivariable que el
+    //la FPGA internamente conecta los diferentes elementos,
+    //en este caso nuestros half_adders son del formato:
+    //"modulo(.arg1(algo) , .arg2(algo) [.....])"
+    //Este tipo de llamadas las hacemos a lo largo de todos
+    //los módulos del proyecto
 
     sumador_1bit HA2 (
         .A(X),
@@ -24,6 +32,9 @@ module full_adder_1bit (
         .C(C2)
     );
 
+    //En vez de hacer un full_adder desde cero, estamos
+    //llamando dos veces al módulo del half_adder en cascada
+    //ya que es una implementación más simple.
     or (Cout, C1, C2);
 
 endmodule
