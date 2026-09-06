b938732ab8324304b79ffbb98469d0e76956921f
v1cc10 <victor.h.parada.m@gmail.com>
2026-09-06 13:28:13 -0300

    Add comments to restador_inverse_4bit module
    Added comments explaining the purpose and behavior of the restador_inverse_4bit module.

--- src/restador_inverse_4bit.v
+++ src/restador_inverse_4bit.v
@@ -5,6 +5,16 @@ module restador_inverso_4bit (
     output Cout
 );
 
+//Básicamente este es el módulo para cambiar el orden de
+//operación de A - B ---> B - A
+//Creo que de este no tenemos un diagrama, pero no debiese
+//ser relevante, ya que combinacionalmente sería el equivalente
+//a cambiar de orden las entradas.
+//Por otra parte acá se puede observar un caso donde se podría
+//generar confusión entre los nombres de los argumentos del módulo
+//que está siendo llamado  y los nombres de las variables que 
+//están siendo utilizadas por el módulo.
+
     restador_4bit RESTA_INV (
         .A(B),
         .B(A),
