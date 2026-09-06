c6819d3ad7620aecf654ed7dee3fc18ff7977534
v1cc10 <victor.h.parada.m@gmail.com>
2026-09-06 13:14:22 -0300

    Comentario sobre el sumador_1bit

--- src/sumador_1bit.v
+++ src/sumador_1bit.v
@@ -4,7 +4,12 @@ module sumador_1bit (
     output S,
     output C
 );
-
+//En efecto si tenemos un sumador de 1 bit que incluye el acarreo de
+//salida, es nuestro half_adder de un solo bit, coincide con la lógica
+//combinacional del Seba, además es importante recordar que los
+//inputs en este módulo son de solamente 1 bit, no son los números
+//reales con los que si queremos operar. Este si mal no recuerdo es
+//el módulo más simple combinacionalmente.
     xor (S, A, B);
     and (C, A, B);
 
