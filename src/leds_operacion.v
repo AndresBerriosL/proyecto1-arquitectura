61be00dd9a9f9a3e2629af486d0f9d7c6653df74
v1cc10 <victor.h.parada.m@gmail.com>
2026-09-07 11:09:55 -0300

    Enhance comments for input and output in leds_operacion
    Added comments to clarify input and output signals.

--- src/leds_operacion.v
+++ src/leds_operacion.v
@@ -1,8 +1,10 @@
 module leds_operacion (
-    input [2:0] operacion,
-    output [2:0] leds
+    input [2:0] operacion, //Operacion seleccionada (3 bits)
+    output [2:0] leds      //Representación con los leds pequeños
 );
-
+    //Asigna el valor de la operacion a los leds,
+    //este módulo es incluso más simple que el 
+    //módulo de sumador_1bit
     buf (leds[0], operacion[0]);
     buf (leds[1], operacion[1]);
     buf (leds[2], operacion[2]);
