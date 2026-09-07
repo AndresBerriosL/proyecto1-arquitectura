a36e764872f8927095b7aced2a98322822b7ec4c
v1cc10 <victor.h.parada.m@gmail.com>
2026-09-06 22:15:16 -0300

    Enhance visualizador_valor_4bit.v with comments
    Added comments to clarify input and output signals.

--- src/visualizador_valor_4bit.v
+++ src/visualizador_valor_4bit.v
@@ -1,18 +1,20 @@
 module visualizador_valor_4bit (
-    input [3:0] valor,
-    output [6:0] display_signo,
-    output [6:0] display_hex
+    input [3:0] valor,      //Número de 4 bits en complemento a 2 que queremos ver
+    output [6:0] display_signo, //Señales para el display que muestra el signo + o -
+    output [6:0] display_hex    //Señales para el display que muestra el valor en hexadecimal
 );
 
     wire signo;
     wire [3:0] magnitud;
 
+    //Se calcula el signo y la magnitud del número de entrada
     magnitud_4bit MAG (
         .valor(valor),
         .signo(signo),
         .magnitud(magnitud)
     );
 
+    //Cada segmento del display (a-g) se conecta a un bit del bus display_signo
     display_signo_7seg SIGNO (
         .signo(signo),
         .a(display_signo[6]),
@@ -24,6 +26,7 @@ module visualizador_valor_4bit (
         .g(display_signo[0])
     );
 
+    //Cada segmento del display (a-g) se conecta a un bit del bus display_hex
     display_hex_7seg HEX (
         .valor(magnitud),
         .a(display_hex[6]),
